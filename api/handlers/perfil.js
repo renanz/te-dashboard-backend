3634c76d9aac83b0be29dcba17efbe365572d5c9
renanz <renan_zelaya@hotmail.com>
2018-11-01 23:39:20 -0600

    Editar perfil done

--- api/handlers/perfil.js
+++ api/handlers/perfil.js
@@ -1,57 +1,104 @@
-const Perfil = require('../models/perfiles');
+const Perfil = require("../models/perfiles");
 
 module.exports.update = {
   handler: function(req, reply) {
     if (!req.params.id) {
-      return reply({ err: "id es un parametro requerido" }).code(400);
+      return reply.response({ err: "id es un parámetro requerido" });
     }
-    let attributes = {};
 
-    if (req.payload.nombre) {
-      attributes.nombre = req.payload.nombre;
-    }
-    if (req.payload.categorias) {
-      attributes.categorias = req.payload.categorias;
-    }
-    if (req.payload.busquedas) {
-      attributes.busquedas = req.payload.busquedas;
-    }
-    if (req.payload.inactivo) {
-      attributes.inactivo = req.payload.inactivo;
-    }
-    Perfil.findByIdAndUpdate(
-      req.params.id,
-      attributes,
-      { new: true },
-      (err, perfil) => {
-        if (err) {
-          return reply(err).code(500);
-        }
-        return reply.response(perfil);
+    Perfil.findById(req.params.id, (findByIdErr, perfil) => {
+      if (findByIdErr) {
+        return reply.response({ findByIdErr });
       }
-    );
+
+      if (perfil.inactivo) {
+        return reply.response({ err: "No existe el perfil" });
+      } else {
+        Perfil.find({ nombre: req.payload.nombre }, (findErr, docs) => {
+          if (findErr) {
+            return reply.response({ findErr });
+          }
+
+          if (docs.length > 0) {
+            return reply.response({ err: "El nombre ya existe" });
+          } else {
+            let attributes = {};
+            console.log(req.payload);
+            if (req.payload.nombre) {
+              attributes.nombre = req.payload.nombre;
+            }
+
+            if (req.payload.categorias) {
+              if (req.payload.categorias.length < 1) {
+                return reply.response({
+                  err: "Debe de seleccionar al menos una categoría"
+                });
+              }
+
+              attributes.categorias = req.payload.categorias;
+            }
+
+            if (req.payload.busquedas) {
+              if (req.payload.busquedas.length < 1) {
+                return reply.response({
+                  err: "Debe de tener al menos un criterio de búsqueda"
+                });
+              }
+
+              attributes.busquedas = req.payload.busquedas;
+            }
+
+            if (req.payload.inactivo) {
+              attributes.inactivo = req.payload.inactivo;
+            }
+
+            Perfil.findByIdAndUpdate(
+              req.params.id,
+              attributes,
+              { new: true },
+              (err, perfil) => {
+                if (err) {
+                  return reply(err).code(500);
+                }
+
+                return reply.response(perfil);
+              }
+            );
+          }
+        });
+      }
+    });
   }
 };
 
 module.exports.delete = {
-  handler: function (req, reply) {
+  handler: function(req, reply) {
     if (!req.params.id) {
-      return reply({ err: 'id es un parámetro requerido' }).code(400);
+      return reply({ err: "id es un parámetro requerido" }).code(400);
     }
-    if (req.params.eliminar === 'true') {
-      Perfil.findByIdAndDelete(req.params.id, (err) => {
+    if (req.params.eliminar === "true") {
+      Perfil.findByIdAndDelete(req.params.id, err => {
         if (err) {
           return reply.response({ err });
         }
-        return reply.response({ msg: `Perfil con id ${req.params.id} eliminado` });
+        return reply.response({
+          msg: `Perfil con id ${req.params.id} eliminado`
+        });
       });
     } else {
-      Perfil.findByIdAndUpdate(req.params.id, { $set: {inactivo: true}}, { new: true }, (err) => {
-        if (err) {
-          return reply.response({ err });
+      Perfil.findByIdAndUpdate(
+        req.params.id,
+        { $set: { inactivo: true } },
+        { new: true },
+        err => {
+          if (err) {
+            return reply.response({ err });
+          }
+          return reply.response({
+            msg: `Perfil con id ${req.params.id} esta ahora inactivo`
+          });
         }
-        return reply.response({ msg: `Perfil con id ${req.params.id} esta ahora inactivo` });
-      });
+      );
     }
   }
 };
