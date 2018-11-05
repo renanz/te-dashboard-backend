415f510a319ce40bd297d7fe3bd6753e4c96ce33
René Rendón <rene.d.rendon@unitec.edu>
2018-11-04 19:34:57 -0600

    Added route and handler to list sources

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const Home = require('./handlers/home');
 const Perfil = require('./handlers/perfil');
+const Fuentes = require('./handlers/fuente');
 
 exports.register = (plugin, options, next) => {
 
@@ -8,6 +9,7 @@ exports.register = (plugin, options, next) => {
     { method: 'GET', path: '/restricted', config: Home.restricted },
     { method: 'GET', path: '/{path*}', config: Home.notFound },
     { method: 'POST', path: '/eliminar-perfil/{id}/{eliminar}', config: Perfil.delete },
+    { method: 'GET', path: '/listar-fuentes', config: Fuentes.get },
   ]);
 
   next();
