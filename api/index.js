5ce0de449d440baf667ef65430a924cf7c9f5170
RDRR7 <rene.d.rendon@unitec.edu>
2018-11-14 23:10:08 -0600

    Update api/index.js
    Co-Authored-By: MendezOscar <oscarito_16m@live.com.mx>

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ exports.register = (plugin, options, next) => {
     { method: 'POST', path: '/editar-perfil/{id}', config: Perfil.update },
     { method: 'POST', path: '/eliminar-perfil/{id}/{eliminar}', config: Perfil.delete },
     { method: 'POST', path: '/crear-perfil', config: Perfil.create },
-    { method: 'GET', path: '/listar-categorias', config: Categorias.listar},
+    { method: 'GET', path: '/listar-categorias', config: Categorias.list},
     { method: 'GET', path: '/listar-perfiles', config: Perfil.find },
     { method: 'GET', path: '/listar-fuentes', config: Fuentes.get },
   ]);
