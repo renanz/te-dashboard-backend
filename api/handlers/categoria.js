8264c048d26cc286b3b88da822d5fd9cbf8f3237
Lala <oscarito_16m@live.com.mx>
2018-11-15 09:14:03 -0600

    Revert "obtener la lista de categorias"

--- api/handlers/categoria.js
+++
@@ -1,7 +0,0 @@
-const categorias = ["business","entertainment","general","health", "science", "sports", "technology"];
-
-module.exports.list = {
-  handler: function (req, reply) {
-    return reply.response(categorias);
-  }
-};
