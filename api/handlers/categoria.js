2c66d61e645c51be934dc03c1343e55d4db8a3a5
RDRR7 <rene.d.rendon@unitec.edu>
2018-11-14 23:09:59 -0600

    Update api/handlers/categoria.js
    Co-Authored-By: MendezOscar <oscarito_16m@live.com.mx>

--- api/handlers/categoria.js
+++ api/handlers/categoria.js
@@ -1,6 +1,6 @@
 const categorias = ["business","entertainment","general","health", "science", "sports", "technology"];
 
-module.exports.listar = {
+module.exports.list = {
   handler: function (req, reply) {
     return reply.response(categorias);
   }
